4b54d3de2fc756a727649eb011a04ddddda560ee
zooteld <zooteld@gmail.com>
2019-08-05 17:31:25 -0500

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -96,7 +96,6 @@ static const struct
 	uint8_t threshold;
 	time_t time;
 } testnet_hard_forks[] = {
-  { 1, 1, 0, 1565043600 }
 };
 static const uint64_t testnet_hard_fork_version_1_till = (uint64_t)-1;
 
@@ -107,7 +106,6 @@ static const struct
 	uint8_t threshold;
 	time_t time;
 } stagenet_hard_forks[] = {
-	{1, 1, 0, 1565043600}
 };
 
 //------------------------------------------------------------------
