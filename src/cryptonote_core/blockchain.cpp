c7f019c12135d5ae83658cb4c3c3316673c4f7d5
zooteld <zooteld@gmail.com>
2019-08-08 21:47:30 -0500

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -84,8 +84,7 @@ static const struct
   { 1, 1, 0, 1482806500 },
   { 2, 21300, 0, 1497657600 },
   { 3, 72000, 0, 1524577218 }, // Roughly the 20th of April.
-  { 4, 208499, 0, 1531762611 }, // Roughly the 23rd of July.
-   { 5, 737090, 0, 1565305200 } // test
+  { 4, 208499, 0, 1531762611 } // Roughly the 23rd of July.
 };
 
 static const uint64_t mainnet_hard_fork_version_1_till = (uint64_t)-1;
@@ -97,11 +96,7 @@ static const struct
 	uint8_t threshold;
 	time_t time;
 } testnet_hard_forks[] = {
-  { 1, 1, 0, 1482806500 },
-  { 2, 6, 0, 1497181713 },
-  { 3, 7, 0, 1522597016 },
-  { 4, 8, 0, 1522597017 },
-  { 5, 10, 0, 1522597018 }
+
 };
 static const uint64_t testnet_hard_fork_version_1_till = (uint64_t)-1;
 
@@ -112,9 +107,7 @@ static const struct
 	uint8_t threshold;
 	time_t time;
 } stagenet_hard_forks[] = {
-	{1, 1, 0, 1482806500},
-	{2, 5150, 0, 1497181713},
-	{3, 103580, 0, 1522540800} // April 01, 2018
+
 };
 
 //------------------------------------------------------------------
